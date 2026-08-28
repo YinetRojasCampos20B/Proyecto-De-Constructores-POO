ea8d409c01cf4c7f05c33cb8dadc5de386299d91
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-08-27 21:47:58 -0500

    Se agregan los constructores de la clase Lugar: uno con los tres parámetros (nombre, dirección y valoración), dos con dos parámetros (nombre y valoración, y nombre y dirección), y dos con un solo parámetro (dirección, y valoración).

--- ConstructoresPOO/src/constructorespoo/Hospital.java
+++ ConstructoresPOO/src/constructorespoo/Hospital.java
@@ -15,4 +15,35 @@ public class Hospital extends Lugar {
     private char nivelComplejidad;
     private String especializacion;
     private boolean tieneHelipuerto;
+    
+    // Constructores de la clase hija Hospital
+    public Hospital(String nombre, String direccion, int valoracion, char nivelComplejidad, String especializacion, boolean tieneHelipuerto) {
+        super(nombre, direccion, valoracion);
+        this.nivelComplejidad = nivelComplejidad;
+        this.especializacion = especializacion;
+        this.tieneHelipuerto = tieneHelipuerto;
+    }
+
+    public Hospital(String nombre, int valoracion, char nivelComplejidad) {
+        super(nombre, valoracion);
+        this.nivelComplejidad = nivelComplejidad;
+    }
+
+    public Hospital(String direccion, String nombre, char nivelComplejidad, String especializacion) {
+        super(direccion, nombre);
+        this.nivelComplejidad = nivelComplejidad;
+        this.especializacion = especializacion;
+    }
+
+    public Hospital(String nombre, char nivelComplejidad, boolean tieneHelipuerto) {
+        super(nombre);
+        this.nivelComplejidad = nivelComplejidad;
+        this.tieneHelipuerto = tieneHelipuerto;
+    }
+
+    public Hospital(String nombre, boolean tieneHelipuerto) {
+        super(nombre);
+        this.tieneHelipuerto = tieneHelipuerto;
+    }
+    
 }
