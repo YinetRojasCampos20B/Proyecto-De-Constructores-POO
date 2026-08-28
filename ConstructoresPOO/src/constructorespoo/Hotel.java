670a943c500d947a161297ff30514d4e45e8d4fe
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-08-27 21:39:26 -0500

    Agregando los métodos constructores para la clase hija Hotel, en donde se reciben los parámetros de la clase madre Lugar y los de Hotel, dos parámetros ponderacion, valoracion y si tiene wifi, o nombre y dirección, y un constructor donde se recibe nombre, ponderación o si tiene wifi

--- ConstructoresPOO/src/constructorespoo/Hotel.java
+++ ConstructoresPOO/src/constructorespoo/Hotel.java
@@ -15,4 +15,28 @@ public class Hotel extends Lugar {
     private String ponderacion;
     private boolean tieneWifi;
     
+    // Constructores de la clase hija Hotel
+    public Hotel(String ponderacion, boolean tieneWifi, String nombre, String direccion, int valoracion) {
+        super(nombre, direccion, valoracion);
+        this.ponderacion = ponderacion;
+        this.tieneWifi = tieneWifi;
+    }
+
+    public Hotel(String nombre, String ponderacion, int valoracion, boolean tieneWifi) {
+        super(nombre, valoracion);
+        this.tieneWifi = tieneWifi;
+        this.ponderacion = ponderacion;
+    }
+
+    public Hotel(String nombre, String ponderacion, boolean tieneWifi) {
+        super(nombre);
+        this.tieneWifi = tieneWifi;
+        this.ponderacion = ponderacion;
+    }
+
+    public Hotel(String nombre, String ponderacion) {
+        super(nombre);
+        this.ponderacion = ponderacion;
+    }
+    
 }
