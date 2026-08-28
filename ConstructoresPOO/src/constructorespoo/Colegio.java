e79f555e15b532d25f2ce7046c2d929f87684e79
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-08-27 21:55:21 -0500

    Se agregan los constructores de la clase Colegio: uno con los seis parámetros (nombre, dirección, valoración, calendario, tipoColegio y tieneConvenioSENA), uno con cuatro parámetros (dirección, valoración, calendario y tieneConvenioSENA), y uno con tres parámetros (valoración, calendario y tipoColegio).

--- ConstructoresPOO/src/constructorespoo/Colegio.java
+++ ConstructoresPOO/src/constructorespoo/Colegio.java
@@ -9,10 +9,32 @@ package constructorespoo;
  * @author rojas
  */
 public class Colegio extends Lugar {
+    
      // Atributos de la clase hija Colegio
 
     private char calendario;
     private String tipoColegio;
     private boolean tieneConvenioSENA;
+    
+    // Constructores de la clase Colegio
+    
+    public Colegio(String nombre, String direccion, int valoracion, char calendario, String tipoColegio, boolean tieneConvenioSENA) {
+        super(nombre, direccion, valoracion);
+        this.calendario = calendario;
+        this.tipoColegio = tipoColegio;
+        this.tieneConvenioSENA = tieneConvenioSENA;
+    }
+    
+    public Colegio(String direccion, int valoracion, char calendario, boolean tieneConvenioSENA) {
+        super(direccion, valoracion);
+        this.calendario = calendario;
+        this.tieneConvenioSENA = tieneConvenioSENA;
+    }
+
+    public Colegio(int valoracion, char calendario, String tipoColegio) {
+        super(valoracion);
+        this.calendario = calendario;
+        this.tipoColegio = tipoColegio;
+    }
     
 }
